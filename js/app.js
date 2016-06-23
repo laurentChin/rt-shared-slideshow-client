859ddee90545e8e1596d42460313190c1671e9dc
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 17:55:04 +0200

    display selected file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 (function(){
-    let fileInput = document.getElementById('file');
+    var fileInput = document.getElementById('file');
+    var previewContainer = document.getElementById('preview');
+
+    fileInput.addEventListener('change', function(){
+        preview(fileInput.files[0]);
+    });
+
+    function preview(file) {
+        var reader = new FileReader();
+
+        reader.onload = function(event) {
+            previewContainer.src = event.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
 })();
