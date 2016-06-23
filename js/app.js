defdf26fc5c69cebcade40a6b3a96d65e3f9f2e8
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 18:48:34 +0200

    implement xhr file upload

--- js/app.js
+++ js/app.js
@@ -11,8 +11,24 @@
 
         reader.onload = function(event) {
             previewContainer.src = event.target.result;
+            upload(file);
         };
 
         reader.readAsDataURL(file);
+    }
+
+    function upload(file) {
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData();
+        xhr.open('POST', 'http://' + window.location.hostname + ':3000/upload', true);
+
+        formData.append('picture', file);
+        xhr.send(formData);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.status === XMLHttpRequest.DONE) {
+                // TODO handle upload success
+            }
+        }
     }
 })();
